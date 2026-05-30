d1cd2cd86124babd6b25062bb5bebdcf90411cfe
chris marstall <marstall@gmail.com>
2026-05-30 12:39:41 -0400

    Initial Commit

---
+++ skysaw/skysawApp.swift
@@ -0,0 +1,17 @@
+//
+//  skysawApp.swift
+//  skysaw
+//
+//  Created by Chris Marstall on 5/30/26.
+//
+
+import SwiftUI
+
+@main
+struct skysawApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
